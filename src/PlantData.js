66145229d4b1346cb5958d690e92b78a2d3b277a
annick975 <niyubahwe.annick975@gmail.com>
2024-06-04 08:21:03 -0700

    Slight Changes

---
+++ src/PlantData.js
@@ -0,0 +1,23 @@
+import plant1 from "./assets/plant1.jpg";
+import plant2 from "./assets/plant2.jpg";
+import plant3 from "./assets/plant3.jpg";
+
+const plantData = [
+  {
+    id: 1,
+    name: "indoor plant",
+    picture: plant1,
+  },
+  {
+    id: 2,
+    name: "plastic plant",
+    picture: plant2,
+  },
+  {
+    id: 3,
+    name: "short plants",
+    picture: plant3,
+  },
+];
+
+export default plantData;
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,9 +1,16 @@
-
+import { Button, Flex, Typography } from "antd";
 
 function ProductList() {
   return (
-    <div>ProductList</div>
-  )
+    <>
+      <Flex>
+        <Typography.Title level={3} className="primary--color">
+          My products
+        </Typography.Title>
+        <Button type="link" className="gray--color"></Button>
+      </Flex>
+    </>
+  );
 }
 
-export default ProductList
+export default ProductList;
